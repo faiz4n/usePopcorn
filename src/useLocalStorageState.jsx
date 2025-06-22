e4043bfbb38b727282ba57be726e066168d88787
Faizan Ali <faizansmusic@email.com>
2025-06-22 19:26:59 +0530

    Fixed: localstorage persistence

--- src/useLocalStorageState.jsx
+++ src/useLocalStorageState.jsx
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedMovies = localStorage.getItem(key);
-    return storedMovies ? JSON.parse(storedMovies) : [];
+    return storedMovies ? JSON.parse(storedMovies) : initialState;
   });
 
   useEffect(
     function () {
-      localStorage.setItem("watched", JSON.stringify(value));
+      localStorage.setItem(key, JSON.stringify(value));
     },
     [value, key]
   );
